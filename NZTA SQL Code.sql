95fd744fa628c4c5278b42acd8addb9c976ec6b5
JavarnTr <javarntromp@gmail.com>
2022-10-26 13:52:42 +1300

    stuff

--- NZTA SQL Code.sql
+++ NZTA SQL Code.sql
@@ -43,6 +43,7 @@ Create Table Vehicle (
 	Conditions VARCHAR(50),
 	Country VARCHAR(20),
 	Value MONEY,
+	PurchaseDate DATE,
 	DriverID INT references Driver(DriverID)
 );
 
@@ -117,6 +118,11 @@ Create Table Driver_Test (
 	InstructorID INT references Instructor(InstructorID)
 );
 
+Create Table Past_Owners (
+	DriverID INT PRIMARY KEY,
+	VehicleID INT,
+	SaleDate INT;
+)
 
 -- Insert sample data 
 Insert into Driver(driverid, name, dob, phone, email, address) 
@@ -143,17 +149,17 @@ Values (1, 'Class 1 Restricted', '2021-12-15', '2026-12-15', 'Cannot carry passe
 		(9, 'Class 1 Restricted', '2022-09-12', '2027-09-12', 'Cannot carry passengers unless accompanied by supervisor. Can only drive without supervisor between 5am and 10pm.', 9),
 		(10, 'Class 6 Learners', '2018-11-03', '2022-11-03', 'Must drive with an L plate. Cannot carry passengers.', 10);
 		 
-Insert into Vehicle(vehicleid, model, make, colour, fueltype, manufactureyear, class, conditions, country, value, driverid)
-Values (1, 'Toyota', 'Corolla', 'Red', 'Regular', '1995', 'Class 1', 'Faulty locks.', 'United States', '$1,500.00', 1), 
-		(2, 'Nissan', 'X-Trail', 'Navy Blue', 'Regular', '2018', 'Class 1', 'None', 'Japan', '$39,000.00', 2),
-		(3, 'Tesla', 'Model S', 'White', 'Electric', '2019', 'Class 1', 'None', 'United States', '$89,000.00', 3),
-		(4, 'Ford', 'Explorer', 'Blue', 'Regular', '2017', 'Class 1', 'Dent in driver-side door.', 'United States', '$32,000.00', 4),
-		(5, 'BMW', '3 Series', 'Black', 'Regular', '2012', 'Class 1', 'None', 'Mexico', '$12,000.00', 5),
-		(6, 'Toyota', 'RAV4', 'Red', 'Regular', '2015', 'Class 1', 'Chipped windshield', 'United States', '$11,000.00', 6),
-		(7, 'Suzuki', 'Swift', 'Yellow', 'Regular', '2009', 'Class 1', 'None', 'New Zealand', '$9,000.00', 7),
-		(8, 'Audi', 'e-tron', 'White', 'Electric', '2019', 'Class 1', 'None', 'Belgium', '$151,900', 8),
-		(9, 'Nissan', 'Pathfinder', 'Gray', 'Regular', '2014', 'Class 1', 'None', 'Japan', '$13,000.00', 9),
-		(10, 'Hyundai', 'Ioniq 5', 'Red', 'Electric', '2021', 'Class 1', 'None', 'South Korea', '$60,000.00', 10);
+Insert into Vehicle(vehicleid, model, make, colour, fueltype, manufactureyear, class, conditions, country, value, purchasedate, driverid)
+Values (1, 'Toyota', 'Corolla', 'Red', 'Regular', '1995', 'Class 1', 'Faulty locks.', 'United States', '$1,500.00', '2009-07-20', 1), 
+		(2, 'Nissan', 'X-Trail', 'Navy Blue', 'Regular', '2018', 'Class 1', 'None', 'Japan', '$39,000.00', '2019-02-11', 2),
+		(3, 'Tesla', 'Model S', 'White', 'Electric', '2019', 'Class 1', 'None', 'United States', '$89,000.00', '2021-12-06', 3),
+		(4, 'Ford', 'Explorer', 'Blue', 'Regular', '2017', 'Class 1', 'Dent in driver-side door.', 'United States', '2017-09-01', '$32,000.00', 4),
+		(5, 'BMW', '3 Series', 'Black', 'Regular', '2012', 'Class 1', 'None', 'Mexico', '$12,000.00', '2014-08-23', 5),
+		(6, 'Toyota', 'RAV4', 'Red', 'Regular', '2015', 'Class 1', 'Chipped windshield', 'United States', '2020-03-04', '$11,000.00', 6),
+		(7, 'Suzuki', 'Swift', 'Yellow', 'Regular', '2009', 'Class 1', 'None', 'New Zealand', '$9,000.00', '2009-07-20', 7),
+		(8, 'Audi', 'e-tron', 'White', 'Electric', '2019', 'Class 1', 'None', 'Belgium', '$151,900', '2022-05-13', 8),
+		(9, 'Nissan', 'Pathfinder', 'Gray', 'Regular', '2014', 'Class 1', 'None', 'Japan', '$13,000.00', '2016-01-12', 9),
+		(10, 'Hyundai', 'Ioniq 5', 'Red', 'Electric', '2021', 'Class 1', 'None', 'South Korea', '$60,000.00', '2021-11-28', 10);
 		
 Insert into Government_Vehicle(governmentvehicleid, purpose, vehicleid)
 Values (1, 'Police', 1),
