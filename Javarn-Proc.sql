18c96aa2ea4dd68168192caa1217c588363f3041
JavarnTr <83565578+JavarnTr@users.noreply.github.com>
2022-10-30 21:38:10 +1300

    stuff

--- Javarn-Proc.sql
+++ Javarn-Proc.sql
@@ -1,4 +1,6 @@
 --Export all past payments
+--Sometimes Postgres returned an error saying it didn't have permissions to access the csv file, but a solution was to move it to 
+--my C drive's tmp folder and changing the code's file path to match.
 COPY (select name, reason, amount, date
 from payments
 inner join driver on driver.driverid = payments.driverid
